ca8a9673d279db3273c38e647135e456c09f3c72
Sinta Dwi Darmawanti <79841368+SintaDwiDarmawanti@users.noreply.github.com>
2021-03-08 09:49:09 +0700

    Add files via upload

---
+++ Materi2/variable.php
@@ -0,0 +1,11 @@
+<?php
+//kita akan membuat sebuah variable dalam php
+$namadepan = "Diah";
+$namabelakang = "Alviatur Rohmania";
+$namabarang = "buah naga";
+$jumlahbarang = 2;
+$harga = 50000;
+
+echo "Nama saya adalah $namadepan $namabelakang. Saya membeli $namabarang sebanyak $jumlahbarang seharga $harga";
+
+?>
